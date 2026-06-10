18ab69159f15b2513aef198d5d96bb05aa3697bb
Ariel Guzman <celesteth3best@gmail.com>
2026-06-10 16:14:18 -0400

    hello world

---
+++ src/client/java/celestered/warriorkittiens/client/WarriorkittiensDataGenerator.java
@@ -0,0 +1,11 @@
+package celestered.warriorkittiens.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WarriorkittiensDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
